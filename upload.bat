8fc9d8f788896696c9585b05c0f9828a1fda53b7
mihai-ciorobitca <mihai.ciorobitca@outlook.com>
2025-01-19 19:03:23 +0200

    Initial commit

--- upload.bat
+++ upload.bat
@@ -1,5 +1,6 @@
 git remote remove origin
 git remote add origin https://github.com/mihai-ciorobitca/school-networkstudio-template.git
+git branch -M test1
 git add .
 git commit -m "Initial commit"
-git push -u origin master
+git push -u origin test1
