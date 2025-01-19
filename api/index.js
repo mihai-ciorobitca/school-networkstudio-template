790f910f674fbf5208481fba7eb44b061980f738
mihai-ciorobitca <mihai.ciorobitca@outlook.com>
2025-01-19 18:57:34 +0200

    Initial commit

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
     res.render('index');
